aa274a755bf23501494ac3592de6f5269f4f7c1b
Ahmad El-Mowafy <ahmadelmowafy4@gmail.com>
2025-09-12 23:52:32 -0500

    Minor changes

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ const quotes = [
 ];
 
 quotes.push(
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Simplicity is prerequisite for reliability."
 );
 
 const generateBtn = document.getElementById("generate");
